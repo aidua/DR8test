35af5d01e6614b457fb0219c1823e216c1d436f1
AID <aid.ukr@gmail.com>
2019-03-23 17:03:27 +0200

    Installed Gulp and Other things

--- www/themes/custom/first_theme/gulpfile.js
+++ www/themes/custom/first_theme/gulpfile.js
@@ -3,28 +3,34 @@ let sass = require('gulp-sass');
 let sourcemaps = require('gulp-sourcemaps');
 let autoprefixer = require('autoprefixer');
 let postcss = require('gulp-postcss');
-let lost = require('lost');
+let minify = require('gulp-minify-css');
+//let jquery = require('jquery');
+//let bootstrap = require('bootstrap-sass');
 
 gulp.task('sass', function() {
   return gulp.src([
-    'assets/scss/generic/generic.scss',
-    'assets/scss/elements/elements.scss',
-    'assets/scss/objects/objects.scss',
-    'assets/scss/components/components.scss',
-    'assets/scss/utilities/utilities.scss',
+    //'scss/_bootstrap-custom',
+    'node_modules/normalize.css/normalize.css',
+    'scss/bootstrap.scss',
+    'scss/styles.scss',
   ])
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      lost(),
+      //bootstrap(),
       autoprefixer()
     ]))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('./assets/css/'));
+    .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch('assets/scss/**', gulp.parallel('sass'));
+  gulp.watch('scss/**', gulp.parallel('sass'));
+});
+
+gulp.task('hello', function(collback) {
+  console.log('Hello log.');
+  collback();
 });
 
 gulp.task('default', gulp.series('sass', 'sass:watch'));
