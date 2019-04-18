e3f05ce156cc536383a7afd6e848d2e8efdf1748
AID <aid.ukr@gmail.com>
2019-04-19 01:59:29 +0300

    Theming in progress: Final

--- www/modules/custom/blog_paragraphs/src/Plugin/paragraphs/Behavior/RemoteVideoBehavior.php
+++ www/modules/custom/blog_paragraphs/src/Plugin/paragraphs/Behavior/RemoteVideoBehavior.php
@@ -30,42 +30,39 @@ use Drupal\paragraphs\ParagraphsBehaviorBase;
  */
 class RemoteVideoBehavior extends ParagraphsBehaviorBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function isApplicable(ParagraphsType $paragraphs_type)
-    {
-        return $paragraphs_type->id() == "video_youtube";
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type) {
+    return $paragraphs_type->id() == "video_youtube";
+  }
 
-    /**
-     * Extends the paragraph render array with behavior.
-     */
-    public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode)
-    {
-        $max_video_width = $paragraph -> getBehaviorSetting($this->getPluginId(),'video_width', 'full');
-        $bem_block = 'paragraph-'.$paragraph->bundle().($view_mode=='default'? '':'-'.$view_mode);
+  /**
+   * Extends the paragraph render array with behavior.
+   */
+  public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
+    $max_video_width = $paragraph->getBehaviorSetting($this->getPluginId(), 'video_width', 'full');
+    $bem_block = 'paragraph-' . $paragraph->bundle() . ($view_mode == 'default' ? '' : '-' . $view_mode);
 
-        $build['#attributes']['class'][] = Html::getClass($bem_block. '--video-width-'. $max_video_width);
-    }
+    $build['#attributes']['class'][] = Html::getClass($bem_block . '--video-width-' . $max_video_width);
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state)
-    {
-        $form['video_width'] = [
-            '#type' => 'select',
-            '#title' => $this->t('Video block width.'),
-            '#description' => $this->t('Maximum width for video block.'),
-            '#options' => [
-                'full' => '100%',
-                '720p' => '720p',
-                '50%' => '50%',
-            ],
-            '#default_value' => $paragraph -> getBehaviorSetting($this->getPluginId(),'video_width', 'full'),
-       ];
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
+    $form['video_width'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Video block width.'),
+      '#description' => $this->t('Maximum width for video block.'),
+      '#options' => [
+        'full' => '100%',
+        '720p' => '720p',
+        '50%' => '50%',
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'video_width', 'full'),
+    ];
 
-        return $form;
-    }
+    return $form;
+  }
 }
